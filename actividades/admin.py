d717abe84f93c144cd35ed9ae3fe13e3b5f07ae3
federico <fedetrus@gmail.com>
2023-08-18 20:02:43 -0300

    Se agrega el choices para porcentaje al modelo de Actividaddes

--- actividades/admin.py
+++ actividades/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import Actividad, ActividadTipo, Estado
+from .models import Actividad, ActividadTipo, EstadoStandar
 
 # Register your models here.
 
 admin.site.register(Actividad)
 admin.site.register(ActividadTipo)
-admin.site.register(Estado)
+admin.site.register(EstadoStandar)
--- actividades/models.py
+++ actividades/models.py
@@ -2,7 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class Estado(models.Model):
+#   
+class EstadoStandar(models.Model):
     nombre = models.CharField(max_length=255)
 
     def __str__(self):
@@ -15,13 +16,29 @@ class ActividadTipo(models.Model):
        return str(self.actividades)
     
 class Actividad(models.Model):
+        
+    porcentaje_actividadd = [
+    [0, '0'],
+    [1, '10'],
+    [2, '20'],
+    [3, '30'],
+    [4, '40'],
+    [5, '50'],
+    [6, '60'],
+    [7, '70'],
+    [8, '80'],
+    [9, '90'],
+    [10,'100'],
+    ]
+        
     id = models.AutoField(primary_key=True, verbose_name='Nro')
     actividad_user = models.ForeignKey(ActividadTipo, on_delete=models.CASCADE, verbose_name='Actividad', null=True, blank=True)
     create = models.DateTimeField(auto_now_add=True, verbose_name='Fecha Creado')
     asignador = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Asignado por', null=True, blank=True, related_name='Actividad_asignado_por')
     asginado = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Asignado', null=True, blank=True, related_name='actividades_asignadas_a')
     inicio = models.DateTimeField(verbose_name='Inicio', blank=True, null=True)
-    estado = models.ForeignKey(Estado, on_delete=models.CASCADE, verbose_name='Estado', null=True)
+    estado = models.ForeignKey(EstadoStandar, on_delete=models.CASCADE, verbose_name='Estado', null=True)
+    porcentaje = models.IntegerField(choices=porcentaje_actividadd, verbose_name='Porcentaje', default=0)
     fin = models.DateTimeField(verbose_name='Fin', blank=True, null=True)
 
     def __str__(self):
